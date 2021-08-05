61cc67f623053798e129a088e57db35af4e4c17e
maariyadiminsky <mariya.github@gmail.com>
2021-08-05 17:52:12 -0600

    created a sign in and sign out button with auth logic

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import { EMAIL } from "../const";
 
+// NOTE: Google API's authentication status persists between page reloads
 class GoogleAuth extends Component {
-    componentDidMount() {
-        console.log(EMAIL);
+    state = { isSignedIn: null };
+
+    componentDidMount () {
         // loads up google api OAuth client library (I only need email for this app)
         window.gapi.load("client:auth2", () => {
             // return a promise so can pass in callback or .then when complete
@@ -13,14 +15,46 @@ class GoogleAuth extends Component {
             }).then(() => {
                 // api is ready to be used so get auth instance
                 this.auth = window.gapi.auth2.getAuthInstance();
+
+                // check if user is signed in on app load
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+
+                // event listener that listens if user has signed in
+                // callback passed in will get the boolean value
+                this.auth.isSignedIn.listen(this.handleOnUserAuthChange);
             });
         });
+    }
+
+    handleOnUserAuthChange = (isUserSignedIn) => {
+        this.setState({ isSignedIn: isUserSignedIn });
+    }
+
+    handleSetUserAuthChange = () => {
+        const { isSignedIn } = this.state;
+
+        // nothing should happen while google api is still loading sign in status
+        if (isSignedIn == null) return;
+
+        // if signed in, then when user clicks they want to sign out 
+        // if signed out, then when user clicks they want to sign in
+        isSignedIn ? this.auth.signOut() : this.auth.signIn();
+    }
+
+    renderButtonForAuthUser() {
+        const { isSignedIn } = this.state;
+
+        if (isSignedIn == null) return "Loading...";
+        return isSignedIn ? "Sign Out" : "Sign In";
     }
 
 
     render() {
         return (
-            <div>Google Auth</div>
+            <button className="ui red google button" onClick={this.handleSetUserAuthChange}>
+                <i className="google icon" />
+                {this.renderButtonForAuthUser()}
+            </button>
         );
     }
 }
