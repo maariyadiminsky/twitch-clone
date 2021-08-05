ab08e29d6189f8951eea8e0770939f920febf03c
maariyadiminsky <mariya.github@gmail.com>
2021-08-05 16:30:02 -0600

    created and used const folder

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import { EMAIL } from "../const";
 
 class GoogleAuth extends Component {
     componentDidMount() {
-        // loads up google api OAuth client library
+        console.log(EMAIL);
+        // loads up google api OAuth client library (I only need email for this app)
         window.gapi.load("client:auth2", () => {
             window.gapi.client.init({
-                clientId: process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID
+                clientId: process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID,
+                scope: EMAIL
             });
         });
     }
