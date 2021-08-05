c7bb0a5b125c192dfcc430e327d1f5d341441987
maariyadiminsky <mariya.github@gmail.com>
2021-08-05 16:09:00 -0600

    created initial components and added Google API

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class GoogleAuth extends Component {
+    render() {
+        return (
+            <div>Google Auth</div>
+        );
+    }
+}
+
+export default GoogleAuth;
