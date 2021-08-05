6b43479de7cdb955000378b33587980f611662bc
maariyadiminsky <mariya.github@gmail.com>
2021-08-05 16:27:19 -0600

    started up google api and entered client id

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 
 class GoogleAuth extends Component {
+    componentDidMount() {
+        // loads up google api OAuth client library
+        window.gapi.load("client:auth2", () => {
+            window.gapi.client.init({
+                clientId: process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID
+            });
+        });
+    }
+
+
     render() {
         return (
             <div>Google Auth</div>
