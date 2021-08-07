2f4de19a13fc2067b34fcad805b2366b813366e3
maariyadiminsky <mariya.github@gmail.com>
2021-08-07 16:55:25 -0600

    creating StreamList UI wip

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getStreams } from "../../actions/streams"
 
-const StreamList = () => {
-    return (
-        <div>
-            StreamList
-        </div>
-    );
-};
+class StreamList extends Component {
+    componentDidMount() {
+        const { getStreams } = this.props;
 
-export default StreamList;
+        getStreams();
+    }
+
+    renderStreams() {
+        const { streams } = this.props;
+
+        return streams.map(({ id, title, description }) => (
+            <div className="item" key={id}>
+                <i className="large middle aligned icon tv" />
+                <div className="content">
+                    {title}
+                    <div className="description">
+                        {description}
+                    </div>
+                </div>
+            </div>
+        ));
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Streams</h2>
+                <div className="ui celled list">
+                    {this.renderStreams()}
+                </div>
+            </div>
+        );
+    }
+}
+
+// turn objects into array for easier mapping
+const mapStateToProps = ({ streams }) => ({
+    streams: Object.values(streams)
+})
+
+export default connect(mapStateToProps, { getStreams })(StreamList);
