6c585381ce74f450bf4c53e976df7a78dd9d6944
maariyadiminsky <mariya.github@gmail.com>
2021-08-07 12:03:50 -0600

    compelted form validation

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,14 +2,22 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends Component {
-    renderInputError = (error, touched) => error && touched && <div>{error}</div>;
-
-    renderInput = ({ label, input, meta: { error, touched } }) => {
+    renderInputError(error) {
         return (
-            <div className="field">
+            <div className="ui error tiny message">
+                <span className="header">{error}</span>
+            </div>
+        );
+    }
+
+    renderInput = ({ label, input, meta: { error, touched }}) => {
+        let hasBeenTouchedAndHasError = touched && error;
+
+        return (
+            <div className={`field ${hasBeenTouchedAndHasError && "error"}`}>
                 <label>{label}</label>
                 <input {...input} autoComplete="off" />
-                {this.renderInputError(error, touched)}
+                {hasBeenTouchedAndHasError && this.renderInputError(error)}
             </div>
         );
     }
@@ -19,7 +27,7 @@ class StreamCreate extends Component {
 
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
                 <Field name="title" component={this.renderInput} label="Enter Title"/>
                 <Field name="description" component={this.renderInput} label="Enter Description" />
                 <button className="ui button large inverted red">Create Stream</button>
