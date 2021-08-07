2eac638b625a6afd347314fac86a57481ea371fe
maariyadiminsky <mariya.github@gmail.com>
2021-08-07 12:45:32 -0600

    hooked up streamsAPI, and added dev and prod urls

---
+++ src/apis/streamsAPI.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+import { DEVELOPMENT, PRODUCTION } from "../const";
+
+const FindAPIUrl = () => {
+    if (process.NODE_ENV === PRODUCTION) {
+        return process.ENV.REACT_APP_DEV_API_URL;
+    } else if (process.NODE_ENV === DEVELOPMENT) {
+        return process.ENV.REACT_APP_PROD_API_URL;
+    }
+}
+
+export default axios.create({
+    baseURL: FindAPIUrl()
+})
--- src/const/index.js
+++ src/const/index.js
@@ -5,3 +5,7 @@ export const LOADING = "Loading...";
 export const EMAIL = "email";
 export const SIGN_IN = "Sign In";
 export const SIGN_OUT = "Sign Out";
+
+// .env
+export const PRODUCTION = "production";
+export const DEVELOPMENT = "development"
