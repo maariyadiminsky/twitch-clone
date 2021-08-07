0653d1c44290b5b4d9c937a9c87be2210ab4b8a0
maariyadiminsky <mariya.github@gmail.com>
2021-08-07 12:51:10 -0600

    added endpoints to const folder

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,16 @@
+import { streamsAPI } from "../apis/streamsAPI";
+import { STREAMS_ENDPOINT } from "../apis/const";
+
 import { SIGN_USER_IN, SIGN_USER_OUT } from "./types";
 
+// api
+export const createStream = (formValues) => (
+    async(dispatch) => {
+        streamsAPI.post(STREAMS_ENDPOINT, formValues)
+    }
+);
+
+// auth
 export const signUserIn = (userId) => {
     return {
         type: SIGN_USER_IN,
---
+++ src/apis/const.js
@@ -0,0 +1,1 @@
+export const STREAMS_ENDPOINT = "/streams";
