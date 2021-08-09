e830168fb84a51a9bfe6230992e4e4ccd6c31c8b
maariyadiminsky <mariya.github@gmail.com>
2021-08-09 15:40:46 -0600

    created basic Modal component utilizing React portals

---
+++ src/components/Modal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Modal = (props) => {
+    return ReactDOM.createPortal(
+        <div className="ui dimmer modals visible active">
+            <div className="ui standard modal visible active">
+                Modal
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
