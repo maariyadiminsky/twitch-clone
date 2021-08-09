933284e45082361affb3137a83bc12bf5b8ed9aa
maariyadiminsky <mariya.github@gmail.com>
2021-08-09 15:54:38 -0600

    added more mock contentto Modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,8 +4,13 @@ import { createPortal } from "react-dom";
 const Modal = () => {
     return createPortal(
         <div className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">
-                Modal
+            <div className="ui tiny modal visible active">
+                <div className="header">Delete Stream</div>
+                <div className="content">Are you sure you want to delete this stream?</div>
+                <div className="actions">
+                    <button className="ui button">Nevermind</button>
+                    <button className="ui red button">Yes, I'm sure</button>
+                </div>
             </div>
         </div>,
         document.getElementById("modal")
